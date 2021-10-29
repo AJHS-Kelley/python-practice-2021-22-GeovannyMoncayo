567e6b5b54bcea293f85e9dcf00209c4851a2618
Geovanny <s11575131@students.duvalschools.org>
2021-10-29 14:47:23 -0400

    version 0.2
    Added print statement for lives_remaining, forty_time, death_cry.

--- variables/variable_practice.py
+++ variables/variable_practice.py
@@ -1,4 +1,4 @@
-# Variables Practice, Geovanny Moncayo, 10/29/21 2:37pm, version 0.1
+# Variables Practice, Geovanny Moncayo, 10/29/21 2:45pm, version 0.2
 
 # Three Common Types of Data
 
@@ -6,3 +6,7 @@ lives_remaining = 17 # INTEGERS, any +/- WHOLE number and 0.
 forty_time = 5.17 # FLOATING POINT, any +/- number with a decimal
 death_cry = "Curse you Perry the platypus"
 # Line 7 is a STRING -- collection of letters/numbers/other chars.
+
+print(lives_remaining)
+print(forty_time)
+print(death_cry)
