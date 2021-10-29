0b5d985878917d48f6a1132449d20977a98b4125
Geovanny <s11575131@students.duvalschools.org>
2021-10-29 14:42:56 -0400

    version 0.1
    Listed the Three Common Types of Data

--- variables/variable_practice.py
+++ variables/variable_practice.py
@@ -1,1 +1,8 @@
-# Variables Practice, Geovanny Moncayo, 10/29/21 2:22pm, version 0.0
+# Variables Practice, Geovanny Moncayo, 10/29/21 2:37pm, version 0.1
+
+# Three Common Types of Data
+
+lives_remaining = 17 # INTEGERS, any +/- WHOLE number and 0.
+forty_time = 5.17 # FLOATING POINT, any +/- number with a decimal
+death_cry = "Curse you Perry the platypus"
+# Line 7 is a STRING -- collection of letters/numbers/other chars.
